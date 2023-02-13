7ff76a4142f2416495220f5b671a89018f595b04
Adrian Manjarres <ICAdrian.Manjarres@americanwell.com>
2023-02-12 22:07:41 -0500

    Add code block component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
+// https://astro.build/config
+import image from "@astrojs/image";
+// https://astro.build/config
+import mdx from "@astrojs/mdx";
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
-import image from "@astrojs/image";
-
-// https://astro.build/config
-import mdx from "@astrojs/mdx";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), mdx()]
+  integrations: [tailwind(), image(), mdx()],
+   markdown: {
+    shikiConfig: {
+      theme: 'one-dark-pro',
+    },
+  }
 });
